d2bfd68031fee9e3ebdca41eb7d3bfc562a48b57
v4dimm <v4dim.mv@yandex.ru>
2021-05-18 12:26:50 -0400

    Added challenge 8 task

---
+++ Set 1/Challenge_8.py
@@ -0,0 +1,36 @@
+from requests import get
+from base64 import b64decode
+from Crypto.Cipher import AES
+
+
+def get_cipher_blocks():
+    text = get('https://cryptopals.com/static/challenge-data/8.txt').text
+    ciphertext = [bytes.fromhex(line.strip()) for line in text.split('\n')]
+    return ciphertext
+
+
+def get_repeating_block(ciphertext, block_size):
+    numbers = []
+
+    for block in ciphertext:
+        chunks = [block[i:i + block_size] for i in range(0, len(block), block_size)]
+        repeating = len(chunks) - len(set(chunks))
+        numbers.append({
+            'repeating': repeating,
+            'block': block
+        })
+    
+    most_repeating_block = sorted(numbers, key=lambda x: x['repeating'], reverse=True)[0]
+    return most_repeating_block
+
+
+def main():
+    block_size = 16
+
+    cipher_blocks = get_cipher_blocks()
+    block = get_repeating_block(cipher_blocks, block_size)
+    return block
+
+
+if __name__ == "__main__":
+    print(main())
